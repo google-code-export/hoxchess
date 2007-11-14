029f7ae630c89d1695a2a3910e5cd53e81a7f084
huyphan74 <huyphan74@df23ac09-443d-0410-ae7d-9518c3ca72dd>
2007-11-14 15:12:01 +0000

    Add Copyright notice to all files. Include a COPYING of the GNU-v3 license file.

--- trunk/CChess/hox_Project/hox_Client/hoxNetworkAPI.h
+++ trunk/CChess/hox_Project/hox_Client/hoxNetworkAPI.h
@@ -1,6 +1,24 @@
+/***************************************************************************
+ *  Copyright 2007 Huy Phan  <huyphan@playxiangqi.com>                     *
+ *                                                                         * 
+ *  This file is part of HOXChess.                                         *
+ *                                                                         *
+ *  HOXChess is free software: you can redistribute it and/or modify       *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  HOXChess is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with HOXChess.  If not, see <http://www.gnu.org/licenses/>.      *
+ ***************************************************************************/
+
 /////////////////////////////////////////////////////////////////////////////
 // Name:            hoxNetworkAPI.h
-// Program's Name:  Huy's Open Xiangqi
 // Created:         10/26/2007
 //
 // Description:     Containing network related APIs specific to this project.
