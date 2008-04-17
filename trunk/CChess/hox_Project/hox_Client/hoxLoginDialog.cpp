c9d04f3e51b1d62856a9941e0b77d667c5e6a40b
huyphan74 <huyphan74@df23ac09-443d-0410-ae7d-9518c3ca72dd>
2008-04-17 13:17:42 +0000

    HOXServer: Changed the static IP to use "games.playxiangqi.com"

--- trunk/CChess/hox_Project/hox_Client/hoxLoginDialog.cpp
+++ trunk/CChess/hox_Project/hox_Client/hoxLoginDialog.cpp
@@ -66,7 +66,7 @@ struct SiteInfo
 static SiteInfo s_siteList[] =
 {
     { hoxSITE_TYPE_CHESSCAPE, "Chesscape", "Chesscape.com", "games.chesscape.com", "3534", "", "" },
-    { hoxSITE_TYPE_REMOTE, "HOXChess", "HOXChess Server", "209.20.66.220", "8000", "", "" },
+    { hoxSITE_TYPE_REMOTE, "HOXChess", "HOXChess Server", "games.playxiangqi.com", "8000", "", "" },
 };
 
 // ----------------------------------------------------------------------------
