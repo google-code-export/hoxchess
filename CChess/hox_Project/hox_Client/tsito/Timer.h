dd84b1e470d50e3dedbb9f22de6fbe7e405f0b14
huyphan74 <huyphan74@df23ac09-443d-0410-ae7d-9518c3ca72dd>
2008-09-13 15:34:01 +0000

    Code cleanup TSITO AI (continued.)

--- CChess/hox_Project/hox_Client/tsito/Timer.h
+++ CChess/hox_Project/hox_Client/tsito/Timer.h
@@ -6,46 +6,36 @@
  * Timer classes keep track of game clocks including move timers and total game clocks.
  */
 
-#include	<string>
-#include	"Board.h" // Definition of color.
-
-#include	<ctime>
+#include <ctime>
 
 class Timer
 {
 private:
-  static Timer *_redTimer;
-  static Timer *_blueTimer;
+    int	    _moves;
+    int	    _movesLeft;
 
-  int	_moves;
-  int	_movesLeft;
+    int	    _seconds;
+    int	    _secondsLeft;
+    int     _increment;
 
-  int	_seconds;
-  int	_secondsLeft;
-  int	_increment;
-
-  time_t	_startTime;
-  time_t	_endTime;
-
-  Timer(int moves, int seconds, int increment);
+    time_t	_startTime;
+    time_t	_endTime;
 
 public:
-  static void setTimers(int moves, int seconds, int increment);
-  static Timer* timerForColor(color c);
-  static void resetTimers() { _redTimer->resetTimer(); _blueTimer->resetTimer(); }
+    Timer();
+    void    setupTimer(int moves, int seconds, int increment);
 
-  bool	haveTimeLeftForMove();
-  void 	moveMade();
-  void	startTimer();
-  void	stopTimer();
-  void	resetTimer();
+    bool	haveTimeLeftForMove();
+    void 	moveMade();
+    void	startTimer();
+    void	stopTimer();
+    void	resetTimer();
 
-  bool	surpasedTime();
+    bool	surpasedTime();
 
-  int movesLeft() { return _movesLeft;   }
-  int timeLeft();
-  bool active() { return !(_moves == 0 && _seconds == 0); }
+    int movesLeft() { return _movesLeft;   }
+    int timeLeft();
+    bool active() { return !(_moves == 0 && _seconds == 0); }
 };
-
 
 #endif /* __TIMER_H__ */
