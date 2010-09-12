5c087fade42382a9ad35ab6237a99410e4a400c0
huyphan74 <huyphan74@df23ac09-443d-0410-ae7d-9518c3ca72dd>
2010-09-12 23:35:20 +0000

    Update the Copyright information.

--- trunk/iPhone/Classes/NetworkConnection.m
+++ trunk/iPhone/Classes/NetworkConnection.m
@@ -1,20 +1,20 @@
 /***************************************************************************
- *  Copyright 2009-2010 Nevo Hua  <nevo.hua@playxiangqi.com>               *
+ *  Copyright 2010-2011 Huy Phan <huyphan@playxiangqi.com>                 *
  *                                                                         * 
- *  This file is part of NevoChess.                                        *
+ *  This file is part of HOXChess.                                         *
  *                                                                         *
- *  NevoChess is free software: you can redistribute it and/or modify      *
+ *  HOXChess is free software: you can redistribute it and/or modify       *
  *  it under the terms of the GNU General Public License as published by   *
  *  the Free Software Foundation, either version 3 of the License, or      *
  *  (at your option) any later version.                                    *
  *                                                                         *
- *  NevoChess is distributed in the hope that it will be useful,           *
+ *  HOXChess is distributed in the hope that it will be useful,            *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
  *  GNU General Public License for more details.                           *
  *                                                                         *
  *  You should have received a copy of the GNU General Public License      *
- *  along with NevoChess.  If not, see <http://www.gnu.org/licenses/>.     *
+ *  along with HOXChess.  If not, see <http://www.gnu.org/licenses/>.      *
  ***************************************************************************/
 
 #import "NetworkConnection.h"
